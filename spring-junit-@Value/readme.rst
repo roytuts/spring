d739f6e11ccea11369030a94c751ce636924ff0f
Soumitra Roy Sarkar <projwebinfo@gmail.com>
2019-06-25 16:51:35 +0530

    Add files via upload

---
+++ spring-junit-@Value/readme.rst
@@ -0,0 +1,1 @@
+You can go through the tutorial https://www.jeejava.com/mock-an-autowired-value-field-in-spring-with-junit-mockito/
---
+++ spring-junit-@Value/src/test/java/com/jeejava/junit/value/service/SpringJunitValueTest.java
@@ -0,0 +1,28 @@
+package com.jeejava.junit.value.service;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.test.util.ReflectionTestUtils;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SpringJunitValueTest {
+
+	@Spy
+	private final SpringJunitService springJunitService = new SpringJunitService();
+
+	@Before
+	public void setUp() {
+		ReflectionTestUtils.setField(springJunitService, "securityKey", "it's a security key");
+	}
+
+	@Test
+	public void testUpdateUser() throws Exception {
+		springJunitService.updateValue();
+		Mockito.verify(springJunitService, Mockito.times(1)).updateValue();
+	}
+
+}
